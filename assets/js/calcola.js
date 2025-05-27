dff32d2c1a8ac15e16a1226302f6aab3925f2d67
Artem Bulgakov <artem.bulgakov@edu-its.it>
2025-05-27 11:10:20 +0200

    Restyle and optimize all pages

--- assets/js/calcola.js
+++ assets/js/calcola.js
@@ -59,3 +59,11 @@ Your Developer Artem Bulgakov`);
     // form.submit();
   });
 });
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('nav');
+
+navToggle.addEventListener('click', () => {
+  const expanded = navToggle.getAttribute('aria-expanded') === 'true' || false;
+  navToggle.setAttribute('aria-expanded', !expanded);
+  nav.classList.toggle('active');
+});
